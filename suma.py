e535b15c10874cf57e798e0ed0cc42503c7b1b54
Pinto <160087007+SGPinto@users.noreply.github.com>
2025-05-05 23:10:45 -0500

    Create suma.py
    First exercise

---
+++ suma.py
@@ -0,0 +1,14 @@
+########################################################
+#ESTE PROGRAMA SUMA DOS NUMEROS INGRESADOS POR EL USUARIO
+
+#SE PIDE EL PRIMERO NUMERO Y SE CONVIERTE A FLOAR (NUMERO CON DECIMALES)
+print ("Hola estas en el Programa de suma de Numeros")
+num1 = float(input("INSERTE EL PRIMER NUMERO"))
+#SE PIDE EL SEGUNDO NUMERO Y SE COVIERTE TAMBIENA FLOAT
+num2 = float(input("INSERTE EL SEGUNDO NUMERO"))
+
+#SE REALIZA LA SUMA
+suma = num1 + num2
+
+#SE MUESTRA EL RESULTADO
+print("LA SUMA ES ", suma)
